eef577c39fcf14722c83830bed69f74faad626b8
prcraoyi <prcraoyi@qq.com>
2018-08-08 17:08:59 +0800

    add golang

---
+++ go/HelloWorld2.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+func main() {
+	who :="world"
+	if len(os.Args) > 1 {
+		who = strings.Join(os.Args[1:], " ")
+	}
+	fmt.Println("Hello", who+"!")
+}
